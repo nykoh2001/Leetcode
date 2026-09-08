4ecb0276b7a1853f51eb761a38552494725fa637
nykoh2001 <nykoh2001@gmail.com>
2026-09-08 14:58:39 +0900

    LRU cache, ordered dict

---
+++ DS/cache/146_LRU.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            value = self.cache[key]
+            self.cache.move_to_end(key)
+            return value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            self.cache[key] = value
+            return
+
+        if self.capacity == len(self.cache):
+            self.cache.popitem(last=False)
+            self.cache[key] = value
+            return
+
+        self.cache[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
